611a7a3e7f5838bf110bc3dfc155d8a81a6fa539
Stephan Plepelits <skunk@cg.tuwien.ac.at>
2013-04-13 08:22:32 +0200

    Overlay PT: Infrastructure for compiling stylesheet

--- mcp.php
+++ mcp.php
@@ -1,6 +1,25 @@
 <?
+function overlay_pt_compile($path) {
+  copy("{$path}/overlay_pt.template", "{$path}/overlay_pt.mapnik");
+
+  print "Compiled\n";
+}
+
 function overlay_pt_init($renderd) {
+  $compile=false;
+
   $prefix=modulekit_file("overlay_pt", "overlay_pt", true);
+  $path=modulekit_file("overlay_pt", "", true);
+
+  if(!file_exists("$prefix.mapnik"))
+    $compile=true;
+  elseif(filesize("$prefix.mapnik")<1024)
+    $compile=true;
+
+  if($compile) {
+    print "Recompiling\n";
+    overlay_pt_compile($path);
+  }
 
   $renderd['overlay_pt']=array("file"=>"$prefix.mapnik");
 }
